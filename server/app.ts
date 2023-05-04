b920667752fe623cc826f82460f0cd26e12eae53
vitaliyeb <vlvlv.vovov@bk.ru>
2023-05-04 14:29:46 +0300

    add ts

--- server/app.ts
+++ server/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    res.send('hello world 1101222')
+    res.send('hello world 1')
 })
 
 app.listen(port, () => {
